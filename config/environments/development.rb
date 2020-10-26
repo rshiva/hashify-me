7d7ea4ade58ed5dd9d0b984e622afc9ce750e321
Shiva kumar <nadarshiva.r@gmail.com>
2020-10-26 20:02:01 +0530

    Update: hiding body param in log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,12 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.filter_parameters += [:body]
+
+  
+
+  
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
