0495b3424f1dde0d1cd804a39582f940b652c2cf
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-11-01 13:47:46 +1100

    add procfile

--- app/javascript/packs/components/home.tsx
+++ app/javascript/packs/components/home.tsx
@@ -14,9 +14,10 @@ export const Home: React.FC = () => (
         </div>
         <div className="w-1/2 flex items-center justify-center">
           <p>
-            It's really easy to share secrets on your favourite chatting software. However, every time
-            you do it, it lives in the database of those chat companies forever, exposing you to potential
-            security risks.
+            It's really easy to share secrets on your favourite chatting
+            software. However, every time you do it, it lives in the database of
+            those chat companies forever, exposing you to potential security
+            risks.
           </p>
         </div>
       </div>
@@ -32,7 +33,11 @@ export const Home: React.FC = () => (
                 </label>
               </div>
               <div className="md:w-2/3">
-                <textarea className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-middle-blue-green" placeholder="Your secret" cols={20} />
+                <textarea
+                  className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-middle-blue-green"
+                  placeholder="Your secret"
+                  cols={20}
+                />
               </div>
             </div>
             <div className="md:flex md:items-center mb-6">
