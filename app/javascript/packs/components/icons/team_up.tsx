0fef0ddc5ee5a1d062e27040bd75d3f9ded46cb4
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2021-01-11 21:58:17 +1100

    cleanup icons

--- app/javascript/packs/components/icons/team_up.tsx
+++ app/javascript/packs/components/icons/team_up.tsx
@@ -5,6 +5,7 @@ export const TeamUp: React.FC = () => {
     <svg
       data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
+      width=''
       height={462.415}
       viewBox="0 0 802 462.415"
     >
--- app/javascript/packs/components/icons/unlock.tsx
+++ app/javascript/packs/components/icons/unlock.tsx
@@ -5,6 +5,7 @@ export const Unlock: React.FC = () => {
     <svg
       data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
+      width=''
       height={462.415}
       viewBox="0 0 1010.892 727.284"
     >
