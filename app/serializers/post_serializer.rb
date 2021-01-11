954ed8cc8d30b15f44d50645063bef904488f662
Shiva kumar <nadarshiva.r@gmail.com>
2021-01-11 09:12:15 +0530

    added has_salt in the post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 class PostSerializer
   include JSONAPI::Serializer
-  attributes :url_token, :expired_at
+  attributes :url_token, :expired_at, :has_salt
   
   # set_type :salty_password
   set_id :url_token
