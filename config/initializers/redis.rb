24ceecdedd3e93e78ded50a910171b172f96f82b
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2021-01-14 21:42:33 +1100

    fix typo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 uri = URI.parse(Rails.application.config_for(:redis)[:host])
-$redis = Redis.new(host: uri.host, port: url.port, password: uri.password,
+$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password,
                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
                   timeout: 10)
