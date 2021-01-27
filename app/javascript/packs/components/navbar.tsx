6fa05eecfd6ead12953fb464351656010ddc4388
Shiva kumar <nadarshiva.r@gmail.com>
2021-01-27 15:32:28 +0530

    update: share on twitter link class added

--- app/javascript/packs/components/navbar.tsx
+++ app/javascript/packs/components/navbar.tsx
@@ -52,7 +52,9 @@ export const Navbar: React.FC = () => (
         >
           Contact
         </NavLink>
-        <a href="https://twitter.com/share?text=Share Sensitive Data Securely with @hashify_app&url=Hashify.app" target="_blank">
+        <a href="https://twitter.com/share?text=Share Sensitive Data Securely with @hashify_app&url=Hashify.app" 
+           target="_blank"
+           className="text-xl block mt-4 lg:inline-block lg:mt-0 font-title hover:text-white mr-4 navbar-link">
         Share on Twitter</a>
       </div>
     </div>
