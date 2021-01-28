63377fc1ce8dd054809f9169267d47f8971bf70d
Shiva kumar <nadarshiva.r@gmail.com>
2021-01-28 22:34:17 +0530

    condition statement fix

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -3,7 +3,7 @@ class SlackController < ApplicationController
   def authorization
     slack = SlackApiClient.new()
     response = slack.authorize(code: params[:code])
-    if response
+    if response["ok"]
       redirect_to root_url, notice: "Hashify bot added successfully"
     else
       redirect_to root_url, notice: response["error"]
