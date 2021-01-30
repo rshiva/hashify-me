241688b80166344981942bf0ac123aef1807a257
Shiva kumar <nadarshiva.r@gmail.com>
2021-01-30 12:34:47 +0530

    better logger msg

--- app/controllers/Api/v1/slack_controller.rb
+++ app/controllers/Api/v1/slack_controller.rb
@@ -26,7 +26,7 @@ class Api::V1::SlackController < ApplicationController
       # unable to append channed_id, instead sending the private message to hashify. shouldnt the link be avaliable in private chat?
       url = "https://"+request.subdomains[0]+"."+request.domain + "/secret/#{post_hash[:data][:id]}"
       res = slack.post_message(channel: Rails.application.config_for(:slack)[:channel_id],text: "Use this link to share the secret message created.\n <#{url}| Secret URL>")
-      Rails.logger.info("slash command response  --> #{res.inspect}")
+      Rails.logger.info("slash create post response  --> #{res.inspect}")
       head :ok
     end
     
