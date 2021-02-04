9b4fd97f7020e78843666bab74e7aba1e349e34f
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2021-02-04 22:07:24 +1100

    privacy policy

--- app/javascript/packs/components/app.tsx
+++ app/javascript/packs/components/app.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import { Contact } from './contact'
 import { Home } from './home'
 import { Post } from './post'
+import { PrivacyPolicy } from "./privacy_policy"
 
 class App extends React.Component {
     render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
                     <Route exact path="/" component={Home} />
                     <Route exact path="/contact" component={Contact} />
                     <Route exact path="/secret/:slug" component={Post} />
+                    <Route exact path="/privacy-policy" component={PrivacyPolicy} />
                 </Switch>
             </div>
         )
