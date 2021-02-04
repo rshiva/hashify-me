68f841de2f0861a06b0090b8d3f63857f2f66c5d
Shiva kumar <nadarshiva.r@gmail.com>
2021-02-04 23:59:35 +0530

    update: read access token dynamically for respective team_id also test reinstall the app in workspaces

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -5,12 +5,23 @@ class SlackController < ApplicationController
     response = slack.authorize(code: params[:code])
     Rails.logger.info("add to slack authorization response --> #{response.inspect}")
     if response["ok"]
+      # Rails.logger.info("slack logged in user #{response['authed_user']['id']}")
+      add_to_slack_bot_access(response)
       redirect_to root_url, notice: "Hashify bot added successfully"
     else
       redirect_to root_url, notice: response["error"]
     end
   end
 
-  def index
+  private
+
+  def add_to_slack_bot_access(response)
+    if response['team']['id'] && response['access_token']
+      @slack_access = SlackAccessToken.find_by(team_id: response['team']['id'])
+        unless @slack_access.bot_token == response['access_token']
+          @slack_access.bot_token = response['access_token']
+          @slack_access.save!
+        end
+    end
   end
 end
---
+++ spec/factories/slack_access_tokens.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :slack_access_token do
+    team_id { "MyString" }
+    bot_access_token { "MyString" }
+  end
+end
