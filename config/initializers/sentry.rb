c2bb49487943c8e63505518d06f168fbfe7eec6e
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2021-02-06 00:53:47 +1100

    update sentry config

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,12 @@
+Sentry.init do |config|
+    config.dsn = 'https://7f238021f7bb4e2b91dc433a42116095@o513750.ingest.sentry.io/5616172'
+    config.breadcrumbs_logger = [:active_support_logger]
+  
+    # To activate performance monitoring, set one of these options.
+    # We recommend adjusting the value in production:
+    config.traces_sample_rate = 0.5
+    # or
+    config.traces_sampler = lambda do |context|
+      true
+    end
+end
