dda5c2902c9990483281e6af7e5a34227a23c505
Shiva kumar <nadarshiva.r@gmail.com>
2021-04-10 23:10:49 +0530

    downcase group name

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,8 @@ class Group < ApplicationRecord
   has_many :users, :through => :memberships
   belongs_to :admin, class_name: "User", foreign_key: "admin_id"
 
-  
-
   validates :name, presence: true
+
+  before_save {self.name.downcase!}
+
 end
